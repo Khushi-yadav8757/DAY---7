7efe624974e6f83fe87f0926091d0ac3be1e7633
Khushi Yadav <kky957242@gmail.com>
2024-12-23 17:49:03 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,15 @@
+Strings are used for storing text/characters.
+
+For example, "Hello World" is a string.
+
+A string variable contains a collection of characters surrounded by double quotes:
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting = "Hello";
+  cout << greeting;
+  return 0;
+}
